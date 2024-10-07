a0ee6c04212b36e34be8d530d79c12316d5c51d0
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-07 13:05:51 +0530

    done with the code till logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
+import './index.css';
+import { BrowserRouter, Routes,Router, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Header from './Components/Header';
 
-function App () {
+function App() {
   return (
-    <>
-      <h1>Hello ecommerce</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 
-const Header = () =>{
-    return(
+
+
+const Header = () => {
+    return (
         <>
+         <div className='headerwrapper'>
+                <div className='top-strip bg-purple'>
+                    <div className='container'>
+                        <p className='mb-0 mt-0 text-center'>
+                            Due to <b>high amount of request</b>, orders may be processed with a slight delay
+                        </p>
+                    </div>
+                </div>
+            </div>
 
+            <div className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="logowrapper d-flex align-items-center col-sm-2">
+                             <Link to={'/'}> <img src={logo} alt='logo' /> </Link> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+           
         </>
-    )
-}
+    );
+};
+
 export default Header;
