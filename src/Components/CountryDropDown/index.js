6bcf8adda0f74570d078e884a884a877415b67b3
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-13 01:18:04 +0530

    added search bar to the country drop down comp

--- src/Components/CountryDropDown/index.js
+++ src/Components/CountryDropDown/index.js
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 import { FaAngleDown } from "react-icons/fa6";
 import Dialog from '@mui/material/Dialog'; // Import Dialog
 import DialogTitle from '@mui/material/DialogTitle';
+import { IoSearch } from "react-icons/io5";
+
 
 const CountryDropDown = () => {
     return (
@@ -25,11 +27,42 @@ const CountryDropDown = () => {
             </Button>
 
             <Dialog open={true} className='locationModal'>
-                <h5>Choose your Delivery Location</h5>
+                <h4>Choose your Delivery Location</h4>
                 <p>Enter your address and we will specify the offer of your area</p>
+
+                {/* search box code  start*/}
+                <div className="headerSearch d-flex align-items-center ml-3">
+                                    <div className="input-group" style={{ width: '450px', height: '50px' }}>
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            placeholder="Search location" 
+                                            aria-label="Search items"
+                                            style={{ 
+                                                backgroundColor: '#f3f4f7', 
+                                                height: '50px',
+                                                border: '1px solid #ced4da',
+                                                borderRight: 'none',
+                                                borderRadius: '0.25rem 0 0 0.25rem'
+                                            }}
+                                        />
+                                        <div className="input-group-append">
+                                            <span className="input-group-text" style={{ 
+                                                backgroundColor: '#f3f4f7', 
+                                                height: '50px', 
+                                                border: '1px solid #ced4da', 
+                                                borderLeft: 'none', 
+                                                borderRadius: '0 0.25rem 0.25rem 0' 
+                                            }}>
+                                                <IoSearch />
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                {/* search box code end */}
             </Dialog>
         </>
     );
-};
+};  
 
 export default CountryDropDown;
