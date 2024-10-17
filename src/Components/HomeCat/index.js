0dd4c9bcdbf08b7dad7807fcead451f8f5f120b8
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-17 10:12:40 +0530

    added home cat slider with that navigation icon

--- src/Components/HomeCat/index.js
+++ src/Components/HomeCat/index.js
@@ -19,16 +19,348 @@ const HomeCat = ()=>{
                     modules={[Navigation]} // Add Swiper's Pagination module
                    className='mySwiper'>
                                 <SwiperSlide>
-    <div className='item'>
+    <div className='item text-center'>
         <img
             className="img-fluid" // Bootstrap class for responsive images
             src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
             alt="Red Apple"
             style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
         />
-        <h4>Red Apple</h4>
+        <h6>Red Apple</h6>
     </div>
 </SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
+<SwiperSlide>
+    <div className='item text-center'>
+        <img
+            className="img-fluid" // Bootstrap class for responsive images
+            src='https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/6c05243d-4d5d-4427-ae31-f08e14d60818.jpg?ts=1724672692'
+            alt="Red Apple"
+            style={{ width: '100px', height: '100px' }} // Inline CSS for specific width and height
+        />
+        <h6>Red Apple</h6>
+    </div>
+</SwiperSlide>
+
 
                     </Swiper>
             </div>
