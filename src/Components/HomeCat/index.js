225d2f24b5244a5f81220dfbb06a362cac863a7a
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-10-17 10:01:57 +0530

    added swiper section to this component

--- src/Components/HomeCat/index.js
+++ src/Components/HomeCat/index.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation'; // Swiper's pagination CSS
-import { Navigation } from 'swiper/modules'; // Correct import for Swiper's Pagination module
+import { Navigation } from 'swiper/modules'; // Correct import for Swiper's navigation module
 
 const HomeCat = ()=>{
     return(
-        <>
+     
         <section className='homeCat'>
             <div className="container">
-                    <h1>Hello</h1>
+            <Swiper slidesPerView={3}
+                    spaceBetween={30}
+                    pagination={{
+                    clickable: true,
+                   }}
+                    modules={[Pagination]} // Add Swiper's Pagination module
+                   className='mySwiper'>
+                                <SwiperSlide>
+
+                                </SwiperSlide>
+
+                    </Swiper>
             </div>
         </section>
-        </>
+     
     )
 };
 
